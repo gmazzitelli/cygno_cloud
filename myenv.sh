a47474a66cbd19e583f01e157b8409807c4e5d24
gmazzitelli <giovanni.mazzitelli@lnf.infn.it>
2021-06-24 14:51:33 +0200

    on the way

---
+++ myenv.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+git config --global user.name "Giovanni Mazzitelli"
+git config --global user.email "giovanni.mazzitelli@lnf.infn.it"
+git config --global core.excludesfile "/s3/mazzitel/.gitignore”
+
